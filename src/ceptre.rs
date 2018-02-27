ef8c8db91b59dfdada5124bd503e9041f4b9663b
Tobias Mansfield-Williams <contact@tmwhere.com>
2018-02-27 12:58:32 +0100

    Implement rule_matches_state

--- src/ceptre.rs
+++ src/ceptre.rs
@@ -19,9 +19,10 @@ struct Token {
 
 impl Token {
     fn new(string: &str, depth: i32) -> Token {
-        let is_var = string
-            .chars()
-            .all(|c| c.is_ascii_uppercase() || c.is_ascii_digit());
+        let mut chars = string.chars();
+        let first_char = chars.next();
+        let is_var = first_char.expect("first_char").is_ascii_uppercase()
+            && chars.all(|c| c.is_numeric() || !c.is_ascii_lowercase());
 
         Token {
             string: string.to_string(),
@@ -29,73 +30,190 @@ impl Token {
             is_var,
         }
     }
+
+    fn set_depth(&self, depth: i32) -> Token {
+        Token {
+            string: self.string.clone(),
+            depth,
+            is_var: self.is_var,
+        }
+    }
 }
 
-fn match_variables(
-    input_tokens: &Vec<Token>,
-    pred_tokens: &Vec<Token>,
-) -> Option<Vec<(String, Vec<Token>)>> {
-    let mut pred_token_iter = pred_tokens.iter();
-    let mut result: Vec<(String, Vec<Token>)> = vec![];
+type Phrase = Vec<Token>;
+type Match = (String, Phrase);
 
-    for token in input_tokens.iter() {
-        let pred_token = pred_token_iter.next();
+#[derive(Debug, Eq, PartialEq)]
+struct Rule {
+    inputs: Vec<Phrase>,
+    outputs: Vec<Phrase>,
+}
 
-        if let Some(pred_token) = pred_token {
-            if !token.is_var {
-                if token != pred_token {
-                    return None;
+impl Rule {
+    fn new(inputs: Vec<Phrase>, outputs: Vec<Phrase>) -> Rule {
+        Rule { inputs, outputs }
+    }
+}
+
+// Checks whether the rule's forward and backward predicates match the state.
+// Returns a new rule with all variables resolved, with backwards/side
+// predicates removed.
+fn rule_matches_state(r: &Rule, state: &Vec<Phrase>) -> Option<Rule> {
+    let inputs = &r.inputs;
+    let outputs = &r.outputs;
+
+    // TODO: exit early if we already know that side predicates won't match
+
+    let mut i_i = 0;
+    let mut s_i = 0;
+    let mut s_i0 = 0;
+    let mut states_stack = vec![];
+    let mut variables_matched = vec![];
+    let mut variables_matched_length_stack = vec![];
+
+    loop {
+        let input = &inputs[i_i];
+        let mut extract: Option<Vec<Match>> = None;
+
+        let mut found = false;
+        if is_backwards_pred(input) {
+            // predicate does not match to specific state, so store placeholder index.
+            s_i = state.len();
+            found = true;
+        } else {
+            for (i, s) in state.iter().enumerate().skip(s_i0) {
+                let result = match_variables_with_existing(input, s, &variables_matched);
+
+                if result.is_some() && !states_stack.contains(&i) {
+                    s_i = i;
+                    extract = result;
+                    found = true;
+                    break;
                 }
+            }
+        }
+
+        // once variables have been matched against all forward predicates, check
+        // for compatibility with backwards predicates.
+        if found && i_i == inputs.len() - 1 {
+            for input in inputs.iter() {
+                let backwards = is_backwards_pred(input);
+
+                if backwards {
+                    if let Some(mut extra_matches) =
+                        match_backwards_variables(input, &variables_matched)
+                    {
+                        variables_matched.append(&mut extra_matches);
+                    } else {
+                        found = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if !found {
+            if i_i == 0 {
+                return None;
             } else {
-                let mut matches = vec![pred_token.clone()];
+                loop {
+                    s_i0 = states_stack.pop().expect("states_stack") + 1;
 
-                // clone the iterator, because take_while consumes the final
-                // non-matching item and we want to keep it. otherwise we could
-                // just mutate the original iterator.
-                {
-                    let mut iter_clone = pred_token_iter.clone();
-                    if pred_token.depth > token.depth {
-                        matches.extend(iter_clone.take_while(|t| t.depth > token.depth).cloned());
-                    }
+                    let v_length = variables_matched_length_stack
+                        .pop()
+                        .expect("variables_matched_length_stack");
 
-                    for _ in 0..matches.len() - 1 {
-                        pred_token_iter.next();
+                    assert!(v_length <= variables_matched.len());
+                    variables_matched.drain(v_length..);
+
+                    i_i -= 1;
+
+                    if !(s_i0 >= state.len() - 1 && states_stack.len() > 1) {
+                        break;
                     }
                 }
 
-                let has_existing_matches = if let Some(&(_, ref existing_matches)) =
-                    result.iter().find(|&&(ref t, _)| *t == token.string)
-                {
-                    if *existing_matches != matches {
-                        return None;
-                    }
-
-                    true
-                } else {
-                    false
-                };
-
-                if !has_existing_matches {
-                    result.push((token.string.clone(), matches));
+                if s_i0 >= state.len() - 1 {
+                    return None;
                 }
             }
         } else {
-            return None;
+            // if predicate matches successfully, record state that we can revert to
+            // if subsequent predicates match unsuccessfully.
+            states_stack.push(s_i);
+            s_i0 = 0;
+
+            variables_matched_length_stack.push(variables_matched.len());
+            if let Some(ref extract) = extract {
+                variables_matched.append(&mut extract.clone());
+            }
+
+            i_i += 1;
+
+            let all_matched = i_i == inputs.len();
+
+            if all_matched {
+                let mut forward_concrete = vec![];
+                let mut outputs_concrete = vec![];
+
+                for v in inputs.iter() {
+                    if !is_backwards_pred(v) {
+                        forward_concrete.push(assign_vars(v, &variables_matched));
+                    }
+                }
+
+                for v in outputs.iter() {
+                    outputs_concrete.push(assign_vars(v, &variables_matched));
+                }
+
+                return Some(Rule::new(forward_concrete, outputs_concrete));
+            }
         }
     }
+}
 
-    if let Some(_) = pred_token_iter.next() {
-        return None;
+fn match_backwards_variables(
+    pred: &Vec<Token>,
+    existing_matches: &Vec<Match>,
+) -> Option<Vec<Match>> {
+    let pred = assign_vars(pred, existing_matches);
+
+    evaluate_backwards_pred(&pred).and_then(|eval_result| {
+        match_variables_with_existing(&pred, &eval_result, existing_matches)
+    })
+}
+
+fn assign_vars(tokens: &Vec<Token>, matches: &Vec<Match>) -> Vec<Token> {
+    let mut result: Vec<Token> = vec![];
+
+    for token in tokens.iter() {
+        if token.is_var {
+            if let Some(&(_, ref tokens)) = matches.iter().find(|&&(ref s, _)| *s == token.string) {
+                let var_depth = token.depth;
+                let tokens = tokens.iter().map(|t| t.set_depth(t.depth + var_depth));
+                result.extend(tokens);
+                continue;
+            }
+        }
+
+        result.push(token.clone());
     }
 
-    return Some(result);
+    return result;
+}
+
+fn is_backwards_pred(tokens: &Vec<Token>) -> bool {
+    if tokens.len() == 0 {
+        return false;
+    }
+
+    match tokens[0].string.as_str() {
+        "+" => true,
+        _ => false,
+    }
 }
 
 fn evaluate_backwards_pred(tokens: &Vec<Token>) -> Option<Vec<Token>> {
-    if tokens.len() == 0 {
-        return None;
-    }
-
     match tokens[0].string.as_str() {
         "+" => {
             use std::str::FromStr;
@@ -129,6 +247,73 @@ fn evaluate_backwards_pred(tokens: &Vec<Token>) -> Option<Vec<Token>> {
         }
         _ => None,
     }
+}
+
+fn match_variables(input_tokens: &Vec<Token>, pred_tokens: &Vec<Token>) -> Option<Vec<Match>> {
+    match_variables_with_existing(input_tokens, pred_tokens, &vec![])
+}
+
+fn match_variables_with_existing(
+    input_tokens: &Vec<Token>,
+    pred_tokens: &Vec<Token>,
+    existing_matches: &Vec<Match>,
+) -> Option<Vec<Match>> {
+    let mut pred_token_iter = pred_tokens.iter();
+    let mut result = vec![];
+
+    for token in input_tokens.iter() {
+        let pred_token = pred_token_iter.next();
+
+        if let Some(pred_token) = pred_token {
+            if !token.is_var {
+                if token != pred_token {
+                    return None;
+                }
+            } else {
+                let mut matches = vec![pred_token.clone()];
+
+                // clone the iterator, because take_while consumes the final
+                // non-matching item and we want to keep it. otherwise we could
+                // just mutate the original iterator.
+                {
+                    let mut iter_clone = pred_token_iter.clone();
+                    if pred_token.depth > token.depth {
+                        matches.extend(iter_clone.take_while(|t| t.depth > token.depth).cloned());
+                    }
+
+                    for _ in 0..matches.len() - 1 {
+                        pred_token_iter.next();
+                    }
+                }
+
+                let has_existing_matches = if let Some(&(_, ref existing_matches)) = result
+                    .iter()
+                    .chain(existing_matches.iter())
+                    .find(|&&(ref t, _)| *t == token.string)
+                {
+                    if *existing_matches != matches {
+                        return None;
+                    }
+
+                    true
+                } else {
+                    false
+                };
+
+                if !has_existing_matches {
+                    result.push((token.string.clone(), matches));
+                }
+            }
+        } else {
+            return None;
+        }
+    }
+
+    if let Some(_) = pred_token_iter.next() {
+        return None;
+    }
+
+    return Some(result);
 }
 
 fn tokenize(string: &str) -> Vec<Token> {
@@ -197,7 +382,147 @@ mod tests {
     fn token_test() {
         assert!(!Token::new("tt1", 0).is_var);
         assert!(!Token::new("tT1", 0).is_var);
+        assert!(!Token::new("1", 0).is_var);
+        assert!(!Token::new("1Tt", 0).is_var);
+        assert!(Token::new("T", 0).is_var);
         assert!(Token::new("TT1", 0).is_var);
+        assert!(Token::new("TT1'", 0).is_var);
+    }
+
+    #[test]
+    fn rule_matches_state_truthiness_test() {
+        let mut test_cases = vec![
+            (
+                Rule::new(vec![tokenize("t1 t3 t2"), tokenize("t1 t2 t3")], vec![]),
+                vec![tokenize("t1 t3 t2"), tokenize("t1 t2 t3")],
+                true,
+            ),
+            (
+                Rule::new(vec![tokenize("t1 T2 T3")], vec![]),
+                vec![tokenize("t1 t2 t3")],
+                true,
+            ),
+            (
+                Rule::new(vec![tokenize("t1 T3 T2"), tokenize("t1 T2 T3")], vec![]),
+                vec![tokenize("t1 t2 t3"), tokenize("t1 t2 t3")],
+                false,
+            ),
+            (
+                Rule::new(vec![tokenize("t1 T3 T2"), tokenize("t1 T2 T3")], vec![]),
+                vec![tokenize("t1 t3 t2"), tokenize("t1 t2 t3")],
+                true,
+            ),
+            (
+                Rule::new(vec![tokenize("t1 T1 T2"), tokenize("+ T1 T2 T2")], vec![]),
+                vec![tokenize("t1 1 2")],
+                false,
+            ),
+            (
+                Rule::new(vec![tokenize("t1 T1 T2"), tokenize("+ T1 T2 T2")], vec![]),
+                vec![tokenize("t1 0 2")],
+                true,
+            ),
+            // successful match with backwards predicates at first and last position
+            (
+                Rule::new(
+                    vec![
+                        tokenize("+ T1 T2 T2"),
+                        tokenize("t1 T1 T2"),
+                        tokenize("t3 T3 T4"),
+                        tokenize("+ T3 T4 T2"),
+                    ],
+                    vec![],
+                ),
+                vec![tokenize("t1 0 2"), tokenize("t3 -2 4")],
+                true,
+            ),
+            // unsuccessful match with backwards predicates at first and last position
+            (
+                Rule::new(
+                    vec![
+                        tokenize("+ T1 T2 T2"),
+                        tokenize("t1 T1 T2"),
+                        tokenize("t3 T3 T4"),
+                        tokenize("+ T3 T4 0"),
+                    ],
+                    vec![],
+                ),
+                vec![tokenize("t1 0 2"), tokenize("t3 -2 4")],
+                false,
+            ),
+        ];
+
+        for (rule, state, expected) in test_cases.drain(..) {
+            let result = rule_matches_state(&rule, &state);
+
+            if expected {
+                assert!(result.is_some());
+            } else {
+                assert!(result.is_none());
+            }
+        }
+    }
+
+    #[test]
+    fn rule_matches_state_output_test() {
+        let rule = Rule::new(
+            vec![
+                tokenize("t1 T1 T2"),
+                tokenize("+ T1 T2 T3'"),
+                tokenize("+ T1 T4' T2"),
+            ],
+            vec![tokenize("t3 T3'"), tokenize("t4 T4'")],
+        );
+        let state = vec![tokenize("t1 3 4")];
+        let expected = Rule::new(
+            vec![tokenize("t1 3 4")],
+            vec![tokenize("t3 7"), tokenize("t4 1")],
+        );
+
+        let result = rule_matches_state(&rule, &state);
+
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(), expected);
+    }
+
+    #[test]
+    fn evaluate_backwards_pred_test() {
+        let mut test_cases = vec![
+            (tokenize("+ A 2 3"), Some(tokenize("+ 1 2 3"))),
+            (tokenize("+ 1 B 3"), Some(tokenize("+ 1 2 3"))),
+            (tokenize("+ 1 2 C"), Some(tokenize("+ 1 2 3"))),
+            (tokenize("+ 1 2 4"), None),
+        ];
+
+        for (input, expected) in test_cases.drain(..) {
+            assert_eq!(evaluate_backwards_pred(&input), expected);
+        }
+    }
+
+    #[test]
+    fn assign_vars_test() {
+        let mut test_cases = vec![
+            (
+                tokenize("+ T1 T2 T3"),
+                vec![
+                    ("T1".to_string(), tokenize("1")),
+                    ("T2".to_string(), tokenize("2")),
+                ],
+                tokenize("+ 1 2 T3"),
+            ),
+            (
+                tokenize("T1 (T2 T3)"),
+                vec![
+                    ("T1".to_string(), tokenize("(t11 t12)")),
+                    ("T3".to_string(), tokenize("(t31 (t32 t33))")),
+                ],
+                tokenize("(t11 t12) (T2 (t31 (t32 t33)))"),
+            ),
+        ];
+
+        for (tokens, matches, expected) in test_cases.drain(..) {
+            assert_eq!(assign_vars(&tokens, &matches), expected);
+        }
     }
 
     #[test]
@@ -244,23 +569,26 @@ mod tests {
             ),
         ];
 
-        for (input_tokens, pred_tokens, result) in test_cases.drain(..) {
-            assert_eq!(match_variables(&input_tokens, &pred_tokens), result);
+        for (input_tokens, pred_tokens, expected) in test_cases.drain(..) {
+            assert_eq!(match_variables(&input_tokens, &pred_tokens), expected);
         }
     }
 
     #[test]
-    fn evaluate_backwards_pred_test() {
-        let mut test_cases = vec![
-            (tokenize("+ A 2 3"), Some(tokenize("+ 1 2 3"))),
-            (tokenize("+ 1 B 3"), Some(tokenize("+ 1 2 3"))),
-            (tokenize("+ 1 2 C"), Some(tokenize("+ 1 2 3"))),
-            (tokenize("+ 1 2 4"), None),
-        ];
+    fn match_variables_with_existing_test() {
+        let input_tokens = tokenize("T1 T2 T3");
+        let pred_tokens = tokenize("t1 t2 t3");
+        let existing_matches = vec![("T2".to_string(), tokenize("t2"))];
 
-        for (input, result) in test_cases.drain(..) {
-            assert_eq!(evaluate_backwards_pred(&input), result);
-        }
+        let result = match_variables_with_existing(&input_tokens, &pred_tokens, &existing_matches);
+
+        assert_eq!(
+            result,
+            Some(vec![
+                ("T1".to_string(), tokenize("t1")),
+                ("T3".to_string(), tokenize("t3")),
+            ])
+        )
     }
 
     #[test]
