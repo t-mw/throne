223fef4975235072439bbfdb489466c1b0406c7f
Tobias Mansfield-Williams <contact@tmwhere.com>
2020-02-07 20:27:18 +0100

    Add benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -43,6 +43,25 @@ fn criterion_benchmark(c: &mut Criterion) {
             },
         )
     });
+
+    c.bench_function("update/context3", |b| {
+        b.iter_with_setup(
+            || {
+                // only parse once, otherwise benchmark is affected
+                lazy_static! {
+                    static ref CONTEXT: throne::Context =
+                        throne::Context::from_text(include_str!("increment.throne"))
+                            .with_test_rng();
+                }
+
+                CONTEXT.clone()
+            },
+            |mut context| {
+                context.append_state("#increment");
+                throne::update(&mut context.core, |_: &throne::Phrase| None);
+            },
+        )
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
