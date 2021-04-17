3ddbe63f9646a4719fd1ed2aa1c83a39228a3b29
Tobias Mansfield-Williams <contact@tmwhere.com>
2021-04-17 20:23:24 +0200

    Fix warnings

--- examples/blocks.rs
+++ examples/blocks.rs
@@ -1,11 +1,9 @@
-#[macro_use]
 extern crate lazy_static;
 
 extern crate minifb;
 extern crate pest;
-extern crate rand;
-#[macro_use]
 extern crate pest_derive;
+extern crate rand;
 extern crate regex;
 
 use minifb::{Key, KeyRepeat, Window, WindowOptions};
